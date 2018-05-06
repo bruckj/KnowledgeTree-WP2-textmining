36c77198daaa20baa6dd02c064df301e078b0fa2
viktoriak02 <37596750+viktoriak02@users.noreply.github.com>
2018-05-06 17:19:33 +0200

    Update T5_functions.py

--- lib/T5/T5_functions.py
+++ lib/T5/T5_functions.py
@@ -3,27 +3,22 @@ import os
 import re
 import io
 import json
+from copy import copy, deepcopy
+from mocsok import replaceNumbers
 
-def replaceNumbers(text):
-        numberblocks = re.compile(r'(\[(\d+)\]\–\[(\d+)\])', re.MULTILINE)
-        for line in numberblocks.finditer(text):
-                whole, min_number, max_number = line.groups()
-                rangeList = list(range(int(min_number), int(max_number)+1))
-                b =','.join("["+ str(i)+"]" for i in rangeList)
-                line = re.sub(numberblocks, b, str(line))
-        return text
 
 def referencesFind(txtDir, metaPath):
 
         refs = {}               #python dict, amiben az egymashoz tartozo elemek vannak (id - hivatkozas - szoveg)
         for txt in os.listdir(txtDir):
                 textFile = open(txtDir + txt, "r", encoding='utf-8')
-                text = textFile.read()
                 name = textFile.name
-                replaceNumbers(text)
+                text = textFile.read()
+                text2 = deepcopy(text)
+                text = replaceNumbers(text2)
                 sents = nltk.sent_tokenize(text)
                 replaceNumbers(text)
-                references = re.compile(r'\(\'(\d+)\'\,\s\'(.*?)\'\)', re.MULTILINE)            #egyszerusitettem a regexet, hogy felismerje a egyseges formara hozott referenciakat
+                references = re.compile(r'(\(\'(\d+)\'\,\s\'(.*?)\'\))', re.MULTILINE)            #egyszerusitettem a regexet, hogy felismerje a egyseges formara hozott referenciakat
                 line_list = []                                                                  
                 number_list = []
                 publication_list = []
@@ -33,7 +28,7 @@ def referencesFind(txtDir, metaPath):
                         refs[name] = {}
                                                 
                 for line in references.finditer(text):
-                        number, publication = line.groups()
+                        refz, number, publication = line.groups()
                         number_list.append(number)
                         publication_list.append(publication)
                         #for publication in publication_list:                                   #ha esetlegesen el kene tuntetni a specialis karaktereket, benne hagytam
@@ -42,10 +37,10 @@ def referencesFind(txtDir, metaPath):
                         for number in number_list:
                                 number1 = number.replace(number, "["+number)
                                 number2 = number.replace(number, number+"]")
-                        ref_list.append(line)
+                        ref_list.append(refz)
 
                         if publication not in refs[name]:
-                                refs[name][publication] = {}
+                                refs[name][publication] = []
                                 
                         for count, line in enumerate(sents):
                                 line_list.append(line)
@@ -53,13 +48,14 @@ def referencesFind(txtDir, metaPath):
                 
                                 if (number1 or number2) in line:
                                         if "]" not in line_list[count-1]:
-                                                lineBefore = line_list[count] + line_list[count-1]
+                                                if line_list[count-1] not in ref_list:
+                                                        lineBefore = line_list[count] + line_list[count-1]
                                         else:
                                                 lineBefore = line_list[count]
                                         if lineBefore not in refs[name][publication]:
-                                                refs[name][publication] = lineBefore
-                                 
-                               
+                                                refs[name][publication].append(lineBefore)
+                                        
+                
         textFile.close()
 
         with open('references.json', 'w+') as outfile:
@@ -70,5 +66,3 @@ txtDir = 'C:\\Users\\Lenovo\\Documents\\files\\'
 metaPath = 'C:\\Users\\Lenovo\\Documents\\'
 
 referencesFind(txtDir, metaPath)
-
-
